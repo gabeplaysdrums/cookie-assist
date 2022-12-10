7d59c5831a168cd51f3d09fb3eba57498a3ad705
Gabriel Young <gabeplaysdrums@live.com>
2022-12-10 14:47:42 -0800

    generate install-dev.html

--- main.js
+++ main.js
@@ -12,42 +12,47 @@ const outputDir = './output'
 if (!fs.existsSync(outputDir))
     fs.mkdirSync(outputDir);
 
-var tokens = {};
+function makeInstaller(outputHtml, versionSuffix, compressor) {
+    var tokens = {};
 
-function replaceTokens(data) {
-    Object.keys(tokens).forEach(function(name) {
-        data = data.replaceAll(`{{${name}}}`, tokens[name]);
+    function replaceTokens(data) {
+        Object.keys(tokens).forEach(function(name) {
+            data = data.replaceAll(`{{${name}}}`, tokens[name]);
+        });
+        return data;
+    }
+
+    tokens['VERSION'] = packageJson.version + versionSuffix;
+
+    var data = fs.readFileSync('./assist.template.js', 'utf8');
+    data = replaceTokens(data);
+    fs.writeFileSync(path.join(outputDir, 'assist.js'), data);
+
+    var bookmarklet = null;
+
+    minify({
+        compressor: compressor,
+        input: [
+            './jquery-3.6.1.js',
+            path.join(outputDir, 'assist.js'),
+        ],
+        output: path.join(outputDir, 'assist.min.js'),
+        sync: true,
+        callback: function(err, min) {
+            if (err)
+                throw (`minify failed: ${err}`);
+            
+            const encoded = encodeURIComponent(min);
+            bookmarklet = `javascript:${encoded}`;
+        }
     });
-    return data;
+
+    tokens['BOOKMARKLET_URL'] = bookmarklet;
+
+    var data = fs.readFileSync('./install.template.html', 'utf8');
+    data = replaceTokens(data);
+    fs.writeFileSync(outputHtml, data);
 }
 
-tokens['VERSION'] = packageJson.version;
-
-var data = fs.readFileSync('./assist.template.js', 'utf8');
-data = replaceTokens(data);
-fs.writeFileSync(path.join(outputDir, 'assist.js'), data);
-
-var bookmarklet = null;
-
-minify({
-    compressor: noCompress,
-    input: [
-        './jquery-3.6.1.js',
-        path.join(outputDir, 'assist.js'),
-    ],
-    output: path.join(outputDir, 'assist.min.js'),
-    sync: true,
-    callback: function(err, min) {
-        if (err)
-            throw (`minify failed: ${err}`);
-        
-        const encoded = encodeURIComponent(min);
-        bookmarklet = `javascript:${encoded}`;
-    }
-});
-
-tokens['BOOKMARKLET_URL'] = bookmarklet;
-
-var data = fs.readFileSync('./install.template.html', 'utf8');
-data = replaceTokens(data);
-fs.writeFileSync(path.join(outputDir, 'install.html'), data);
+makeInstaller(path.join(outputDir, 'install.html'), '', uglifyjs);
+makeInstaller(path.join(outputDir, 'install-dev.html'), '-dev', noCompress);
