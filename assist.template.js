1bb73da2e7c5d0a68d37979ccd7bb41c3c0638b9
Gabriel Young <gabeplaysdrums@live.com>
2022-12-10 13:13:14 -0800

    init menu and button to click the big cookie

--- assist.template.js
+++ assist.template.js
@@ -23,7 +23,40 @@
         return;
     }
 
-    //TODO
+    var $assist = $('<div>')
+        .attr('id', 'cookie-assist')
+        .css('display', 'inline-block')
+        .css('padding', '5px')
+        .css('margin', '2px')
+        .css('color', '#fff')
+        .append($(`<b>Cookie Assistant v${version}:</b> `));
+
+    (function(){
+        var interval = null;
+
+        $assist.append(
+            $('<input type="checkbox" id="cookie-assist-click-cookie">')
+                .css('margin-left', '10px')
+                .change(function() {
+                    if ($(this).is(":checked")) {
+                        interval = window.setInterval(function(){
+                            $('#bigCookie').click();
+                        }, 100);
+                    }
+                    else
+                    {
+                        window.clearInterval(interval);
+                    }
+                })
+        );
+    
+        $assist.append(
+            $('<label for="cookie-assist-click-cookie">click big cookie</label>')
+        );
+    })();
+
+    $('#topBar').children().css('display', 'none');
+    $("#topBar").append($assist);
 
     log.info(`Loaded version ${version}`);
 
